b068b226523c6f4a25f3d18f231bc0820e271d5e
jonathan senior <jonosenior@gmail.com>
2018-01-03 14:09:14 +0100

    Add #elicit_move with regex conversion to Player

--- lib/player.rb
+++ lib/player.rb
@@ -12,4 +12,15 @@ class Player
     gets.chomp
   end
 
+  def elicit_move
+    puts "Make your turn"
+    puts "Row first (0 - 7), then column (1-8)"
+    puts "For example '08 to 64'"
+    convert_to_move(gets.chomp)
+  end
+
+  def convert_to_move(input)
+    input.scan(/\d+/).map { |b| b.to_s.chars.map(&:to_i) }
+  end
+
 end
