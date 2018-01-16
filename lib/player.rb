052c99a63ab0b2456ac8cea79e996a672a3f419d
jonathan senior <jonosenior@gmail.com>
2018-01-16 10:43:21 +0100

    Add square does not exist validation which doesn't break the game

--- lib/player.rb
+++ lib/player.rb
@@ -17,15 +17,24 @@ class Player
   def elicit_move
     puts "\n\n#{@name.upcase}"
     puts "Enter move (for example 'b1 to c3'):"
-    convert_to_move(gets.chomp)
+    moves = convert_to_move(gets.chomp)
+    #binding.pry
+    while !moves
+      puts "That square doesn't exist!"
+      puts "\nEnter move (for example 'b1 to c3'):"
+      moves = convert_to_move(gets.chomp)
+    end
+    moves
+    #convert_to_move(gets.chomp)
   end
 
   def convert_to_move(input)
     moves = alphanumeric_pairs(input)
     moves = moves.map { |move| convert(move) }
     #puts "#{moves}"
-    valid_output?(moves)
-    moves
+    # while !valid_output?(moves)
+    #   puts "Not a valid move!"
+    valid_output?(moves) ? moves : false
   end
 
   def convert(input)
@@ -52,15 +61,14 @@ class Player
   end
 
   def valid_output?(moves)
-    raise Exception.new("invalid_input") unless
     moves.length == 2 && moves.each {|move| move.length == 2 && move.each {|row_or_col| (0..8).include?(row_or_col)}}
   end
 
   def elicit_pawn_promotion
-    puts "#{@name.capitalize} PAWN PROMOTION!"
-    puts "Your pawn made it to the last row."
+    puts "\n\nPAWN PROMOTION!"
+    puts "#{@name.capitalize}, your pawn made it to the last row."
     puts "Which piece would you like to promote him to?"
-    puts "Type: Queen, Rook, Bishop or Knight."
+    puts "Choose: Queen, Rook, Bishop or Knight."
     new_class = gets.chomp.capitalize
     while !valid_pawn_promotion_input?(new_class)
       puts "Invalid choice!"
